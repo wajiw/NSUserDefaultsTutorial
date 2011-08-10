7a3a3a93bc2de244496c722335c5f26874ed09ad
Wa-jiw Casey <wcasey@roundarch.com>
2011-08-09 19:49:17 -0500

    Initial Commit

---
+++ NSUserDefaultsTutorial/NSUserDefaultsTutorialAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  NSUserDefaultsTutorialAppDelegate.h
+//  NSUserDefaultsTutorial
+//
+//  Created by Wa-jiw Casey on 8/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSUserDefaultsTutorialAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
